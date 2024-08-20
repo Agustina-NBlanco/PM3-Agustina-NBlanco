404129585f9665363f0df6f1a86bf70081f2d25f
AgusBlanco <agustinanahirblanco@gmail.com>
2024-08-20 19:50:23 -0300

    Finalización del Backend

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,9 +1,13 @@
-import express, { Express} from "express";
+import express, { Express } from "express";
 import router from "./routes/indexRouter";
+import cors from "cors";
+import morgan from "morgan";
 
 const app: Express = express()
 
+app.use(morgan("dev"));
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 
