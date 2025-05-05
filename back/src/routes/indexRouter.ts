fbf4ba4f9c01df8b3debd442b154bb599d5f1dc5
AgusBlanco <agustinanahirblanco@gmail.com>
2025-05-05 11:21:02 -0300

    Preload modificado para evitar errores

--- back/src/routes/indexRouter.ts
+++ back/src/routes/indexRouter.ts
@@ -4,7 +4,7 @@ import appointmentRouter from "./appointmentsRoutes";
 
 const router: Router = Router();
 
-
+router.get("/", (req, res) => res.send("Hello World!"))
 router.use("/users", userRouter)
 router.use("/appointments", appointmentRouter)
 
